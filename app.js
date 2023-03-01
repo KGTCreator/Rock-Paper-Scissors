a365523d0aa083ce60c574972a37433c0f7cf1b1
KGTCreator <524Ktobin@frhsd.com>
2023-03-01 09:40:40 -0500

    Changes to code and style.

---
+++ app.js
@@ -0,0 +1,49 @@
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
+
+    //Start the Game
+    const startGame = () =>{
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () =>{
+introScreen.classList.add('fadeOut');
+match.classList.add("fadeIn");
+        });
+    };
+    //Play Match
+const playMatch = () => {
+    const options = document.querySelectorAll('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+    //computer options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+options.forEach(option=>{
+    option.addEventListener('click', function(){
+        //computer choice
+const computerNumber = Math.floor(Math.random() * 3);
+const computerChoice = computerOptions[computerNumber];
+//here is where we call compare hands
+    });
+});
+
+const compareHands = (playerChoice, computerChoice) =>{
+const winner = document.querySelector('.winner')
+    if(playerChoice === computerChoice){
+
+}
+}
+
+    const computerNumber = Math.floor(Math.random() * 3;)
+}
+
+    //Is call all the inner function
+    startGame();
+    playMatch();
+}
+
+//start the game function
+game();
